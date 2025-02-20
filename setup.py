48e73f15920c000e09ef754842c72825cfd9ccfd
shalini170217 <shalinit.it2023@citchennai.net>
2025-02-20 17:55:15 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI project',
+    version='0.0.0',
+    author='Shalini',
+    author_email='shalinit.it2023@citchennai.net',
+    packages=find_packages(),
+    install_requires=[]
+)
